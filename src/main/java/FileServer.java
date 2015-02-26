78afa75357a290b908a7922780904f4419742749
dheeraj.sachan@aurusnet.com <dheeraj@Y9198>
2015-02-26 10:57:26 +0530

    Fully functional HTTP server

---
+++ src/main/java/FileServer.java
@@ -0,0 +1,64 @@
+import nanoHTTPServer.NanoHTTPD;
+import nanoHTTPServer.ServerRunner;
+import nanoHTTPServer.Status;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+/**
+ * Created by dheeraj on 25/2/15.
+ */
+public class FileServer extends NanoHTTPD {
+    public FileServer() {
+        super(50000);
+    }
+
+    public static class Mystream extends FileInputStream {
+
+        private FileInputStream fileInputStream;
+
+        public Mystream(String s) throws FileNotFoundException {
+            super(s);
+            fileInputStream = new FileInputStream(s);
+        }
+
+        @Override
+        public int read(byte b[], int off, int len) throws IOException {
+            byte[] bytes = new byte[b.length];
+            int k = fileInputStream.read(bytes, off, len);
+            if (k != -1) {
+                for (int j = 0; j < k; j++) {
+                    if (bytes[j] == -128) {
+                        b[j] = bytes[j];
+                    } else {
+                        b[j] = (byte) ~bytes[j];
+                    }
+                }
+            }
+            return k;
+        }
+
+    }
+
+
+    @Override
+    public Response serve(String uri, Method method,
+                          Map<String, String> header, Map<String, String> parameters,
+                          Map<String, String> files) {
+        InputStream fis = null;
+
+        try {
+            fis = new Mystream("/home/dheeraj/dash-junk/manifest1E.mpd");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return new NanoHTTPD.Response(Status.OK, "application/xml", fis);
+    }
+
+    public static void main(String[] args) {
+        ServerRunner.run(FileServer.class);
+    }
+}
